3d53b0dd94f54c8f7d8964ba88a3a9ff286c7e24
Zakaria RACHEDI <raczak.contact@gmail.com>
2019-08-11 11:18:32 +0200

    ping controller test removed from api explorer list

--- server/src/controllers/ping.controller.ts
+++ server/src/controllers/ping.controller.ts
@@ -1,5 +1,5 @@
-import {Request, RestBindings, get, ResponseObject} from '@loopback/rest';
-import {inject} from '@loopback/context';
+import { Request, RestBindings, get, ResponseObject } from '@loopback/rest';
+import { inject } from '@loopback/context';
 
 /**
  * OpenAPI response for ping()
@@ -11,13 +11,13 @@ const PING_RESPONSE: ResponseObject = {
       schema: {
         type: 'object',
         properties: {
-          greeting: {type: 'string'},
-          date: {type: 'string'},
-          url: {type: 'string'},
+          greeting: { type: 'string' },
+          date: { type: 'string' },
+          url: { type: 'string' },
           headers: {
             type: 'object',
             properties: {
-              'Content-Type': {type: 'string'},
+              'Content-Type': { type: 'string' },
             },
             additionalProperties: true,
           },
@@ -31,21 +31,21 @@ const PING_RESPONSE: ResponseObject = {
  * A simple controller to bounce back http requests
  */
 export class PingController {
-  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
+  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) { }
 
   // Map to `GET /ping`
-  @get('/ping', {
-    responses: {
-      '200': PING_RESPONSE,
-    },
-  })
-  ping(): object {
-    // Reply with a greeting, the current time, the url, and request headers
-    return {
-      greeting: 'Hello from LoopBack',
-      date: new Date(),
-      url: this.req.url,
-      headers: Object.assign({}, this.req.headers),
-    };
-  }
+  // @get('/ping', {
+  //   responses: {
+  //     '200': PING_RESPONSE,
+  //   },
+  // })
+  // ping(): object {
+  //   // Reply with a greeting, the current time, the url, and request headers
+  //   return {
+  //     greeting: 'Hello from LoopBack',
+  //     date: new Date(),
+  //     url: this.req.url,
+  //     headers: Object.assign({}, this.req.headers),
+  //   };
+  // }
 }
