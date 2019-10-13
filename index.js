1ae0726b3124f2cce94e89fe4b51e60ebc9ce76e
Zakaria RACHEDI <raczak.contact@gmail.com>
2019-10-13 17:54:54 +0200

    monitoring

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const client = require('./client');
 const serverApp = require('./server/dist/');
+var swStats = require('swagger-stats');
+var apiSpec = require('./server/swagger.json');
 
 client({
   dir: 'client',
   beforeNext: async server => {
     const app = new serverApp.DofapiApplication();
     await app.boot();
+    server.use(swStats.getMiddleware({ swaggerSpec: apiSpec }));
     server.use('/', app.requestHandler);
   },
 });
